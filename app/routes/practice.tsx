b3a4e1ba7a7c3ad2ad4c78e52ffd159f330d7cd7
Wachsman, Joshua <joshua.wachsman@radancy.com>
2026-01-07 08:35:02 -0500

    updated Gallery function to not be a default function

--- app/routes/practice.tsx
+++ app/routes/practice.tsx
@@ -2,7 +2,7 @@ function Profile() {
   return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
 }
 
-export default function Gallery() {
+export function Gallery() {
   return (
     <section>
       <h1>Amazing scientists</h1>
